99348ff40bd755fabd0aecbbe8d4c3ccbf5ba75b
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2022-05-06 19:13:13 +0200

    added basic frontend functionality

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import { useState } from 'react';
+
 import './App.css';
-
-
+import Input from './components/Input.js'
 
 function App() {
+  const [showInput, setshowInput] = useState(false)
   const [todo, setTodo] = useState([])
+  const [InputField, setInputField] = useState('')
 
-
-
-
+  const submitTodo = (submit) =>{
+    setTodo([...todo, submit])
+    setInputField('')
+ 
+  } 
+    
   return (
     <div className='container'>
-        <h1>todo.</h1>
-        <div className='sub-container'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean odio tellus, suscipit non sapien id, ultricies ornare eros. Maecenas ut ultricies velit. Pellentesque fringilla velit nec nibh cursus, ac dictum neque scelerisque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur id viverra augue. Pellentesque dui ex, sodales nec nulla et, commodo laoreet neque. Praesent vel arcu tristique, posuere justo non, ultricies nibh. Aliquam erat lorem, pellentesque porttitor pellentesque id, dignissim et tellus. In tristique eget ex in blandit. Curabitur nisl metus, sagittis in turpis sed, luctus varius nisi.
+        <h1 className='title'>todo.</h1>
+        <div>
+          {todo.map(t => <p>{t}</p>)}
+        </div>
+        <div className='sub-container' onClick={() => setshowInput(true)}>
+          {showInput ? <Input onSubmit={submitTodo} value={InputField} setInputField={setInputField}/> :''}
 
-Morbi sit amet rutrum lectus. Curabitur volutpat enim at viverra commodo. Curabitur at dignissim enim, sed semper purus. Pellentesque congue nisl ac magna pellentesque, eget porttitor sapien lacinia. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus a lacus a est tincidunt suscipit. Morbi ultricies augue at diam dictum molestie. Cras tincidunt ornare neque et lobortis. Nullam orci leo, vulputate non orci vitae, imperdiet dictum diam. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-
-Etiam vitae lectus sapien. Morbi cursus eget ligula sed condimentum. Nam ex nibh, dictum nec nulla vitae, tincidunt pretium lectus. Donec a justo sed sem sagittis pretium a feugiat ex. Nulla facilisi. Donec in augue justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-
-Donec et lorem at magna dignissim vestibulum vitae a eros. Etiam rutrum ultrices elit, vitae euismod mi. Donec venenatis nisl quis gravida pulvinar. Nunc pretium massa a neque maximus iaculis. Nam interdum scelerisque ipsum, in semper ante ultrices et. Nulla dictum, enim at imperdiet hendrerit, orci felis consectetur magna, scelerisque dictum nisl tellus nec orci. Curabitur bibendum ipsum tortor, sit amet sollicitudin lorem pharetra vel. Nunc non tortor at erat facilisis faucibus in vitae dui. Vivamus id blandit dolor. Sed fringilla nec dolor a lobortis. Cras a lorem tempus, sollicitudin sem vel, tempus eros. Ut quis luctus lacus. Phasellus tristique sed magna in posuere. Nunc ut rutrum ipsum, id finibus justo.
-
-Fusce sapien mi, interdum ac feugiat quis, dignissim et tellus. Aenean tincidunt lacus in eros malesuada euismod. Curabitur bibendum libero nec ipsum efficitur mollis. Mauris consectetur dapibus neque, eget molestie tortor semper vel. Nunc ac venenatis sem. Duis vel tellus tellus. Curabitur mattis lobortis lectus, at lobortis sem rhoncus vel. Mauris aliquam augue arcu, vel sollicitudin lorem pulvinar et. Cras ultricies non leo nec pharetra. Duis auctor purus ante, sed luctus felis varius vel. Fusce sed lectus risus. Maecenas malesuada lectus tempus, dictum ligula vel, hendrerit mi. Pellentesque efficitur sed augue id imperdiet. Fusce venenatis ipsum in semper aliquet. Cras euismod, diam ut condimentum congue, elit sem sollicitudin dui, id aliquet tellus magna a leo.
-
-</div>
+        </div>
     </div>
 
   );
