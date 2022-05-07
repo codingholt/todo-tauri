2069f9f2a903152ecd3432c10dd3b170f440b7ec
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2022-05-07 19:59:38 +0200

    add item

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { arrayRemove } from './util/arrRemove.js';
+import { ask } from '@tauri-apps/api/dialog';
 import './App.css';
 import Input from './components/Input.js'
 
@@ -15,15 +16,27 @@ function App() {
   } 
  
   
-  const removeItem = (t) =>{
-
+  const removeItem = async (t) =>{
+   const sure = await ask(`Are you sure you want to delete ${t}?`)
+    if(sure === false){
+      return
+    }
     const newTodo = arrayRemove(todo, t)
 
     setTodo(newTodo)
   }
+
+  const deleteAllTodo = async () =>{
+    const sure = await ask("Are you sure you want to delete all todo's?")
+    if(sure === false){
+      return
+    }
+    setTodo([])
+  }
   return (
     <div className='container'>
         <h1 className='title'>todo.</h1>
+        {todo.length  > 0 ? <div className='delAll' onClick={()  => deleteAllTodo()}><span>delete all todo's</span></div> : ''}
         <div>
           {todo.map(t => <p className='todo-item' onClick={(e) => e.button === 1 && removeItem(t)}>{t}</p>)}
         </div>
