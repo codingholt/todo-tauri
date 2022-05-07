35a987c525a9126276e009c03052ab147dc892f9
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2022-05-07 19:23:38 +0200

    added remove item on middle mouse click

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-
+import { arrayRemove } from './util/arrRemove.js';
 import './App.css';
 import Input from './components/Input.js'
 
@@ -13,12 +13,19 @@ function App() {
     setInputField('')
  
   } 
-    
+ 
+  
+  const removeItem = (t) =>{
+
+    const newTodo = arrayRemove(todo, t)
+
+    setTodo(newTodo)
+  }
   return (
     <div className='container'>
         <h1 className='title'>todo.</h1>
         <div>
-          {todo.map(t => <p>{t}</p>)}
+          {todo.map(t => <p className='todo-item' onClick={(e) => e.button === 1 && removeItem(t)}>{t}</p>)}
         </div>
         <div className='sub-container' onClick={() => setshowInput(true)}>
           {showInput ? <Input onSubmit={submitTodo} value={InputField} setInputField={setInputField}/> :''}
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import { IoClose}  from 'react-icons/io5';
 const Input = ({ onSubmit, value, setInputField }) => {
   return (
     <div className='input-wrapper'>
         <input className='input' type="text" value={value} onChange={(e) => setInputField(e.target.value)} placeholder='write a todo item...' onKeyDown={(e) => e.key === 'Enter' && onSubmit(e.target.value)}>
             </input>
+            <IoClose className='close-icon'/>
     </div>
   )
 }
