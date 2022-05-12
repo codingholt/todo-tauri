39c68430a8e3840d2897b2147918cfff4127554d
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2022-05-12 10:03:38 +0200

    delete todo message fixed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import { useState, useEffect } from 'react';
-
 import { ask } from '@tauri-apps/api/dialog';
-
 import './App.css';
-
 import {  all,
   create,
   remove,
   removeAll} from './util/db.js'
-
 import Input from './components/Input.js'
 
 
@@ -37,20 +33,18 @@ function App() {
  
   
   const removeItem = async (t) =>{
-   const sure = await ask(`Are you sure you want to delete ${t}?`)
-    if(sure === false){
-      return
-    }
-    remove(t)
-    const newTodo = todo.filter(object => {
-      return object.id !== t;
-    });
-    
-   
-    setTodo(newTodo)
-
-
-  }
+    const deleteTodo = todo.find(ele => ele.id === t)
+    const sure = await ask(`Are you sure you want to delete ${deleteTodo.title}?`)
+     if(sure === false){
+       return
+     }
+     remove(t)
+     const newTodo = todo.filter(object => {
+       return object.id !== t;
+     });
+     
+     setTodo(newTodo)
+  } 
 
   const deleteAllTodo = async () =>{
     const sure = await ask("Are you sure you want to delete all todo's?")
@@ -60,13 +54,13 @@ function App() {
     removeAll()
     setTodo([])
   }
-  
+
   return (
     <div className='container'>
         <h1 className='title'>todo.</h1>
         {todo.length  > 0 ? <div className='delAll' onClick={()  => deleteAllTodo()}><span>delete all todo's</span></div> : ''}
         <div>
-          {todo.map(t => <p className='todo-item' key={t.id} onClick={(e) => e.button === 1 && removeItem(t.id)}><span>{t.title}</span></p>)}
+          {todo.map(t => <p className='todo-item' key={t.id} onClick={(e) => e.button === 1 && removeItem(t.id)}><input type="radio" checked={t.done}></input> <span>{t.title}</span></p>)}
         </div>
         <div className='sub-container' onClick={() => setshowInput(true)}>
           {showInput ? <Input onSubmit={submitTodo} value={InputField} setInputField={setInputField}/> :''}
